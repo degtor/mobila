a82dd70a310bf5eb6cafba299ddc32ed59c093e4
Viktor Ceder <zeerdu@gmail.com>
2016-04-26 01:19:08 +0200

    Byggt vidare
    Bytt ut hur videos är hanterade för att det bättre ska funka på mobilen

--- lab2video/script.jsx
+++ lab2video/script.jsx
@@ -11,6 +11,7 @@ var Main = React.createClass({
 
 // Video counter
 var n = 0;
+var pressed = false;
 
 // Child
 var Video = React.createClass({
@@ -36,6 +37,7 @@ var Video = React.createClass({
             video: videos[n].url,
             title: videos[n].title,
             rating: videos[n].rating,
+            thumbnail: videos[n].thumbnail,
             category:videos[n].category,
             comments: videos[n].comments
         });
@@ -52,17 +54,31 @@ var Video = React.createClass({
             video: videos[n].url,
             title: videos[n].title,
             rating: videos[n].rating,
+            thumbnail: videos[n].thumbnail,
             category:videos[n].category,
             comments: videos[n].comments
         });
     },
-
+    allVideo: function () {
+        if (pressed) {
+            pressed = false;
+        }else {
+            pressed = true;
+            ShowAllVideos;
+        }
+    },
     render: function() {
         return (
             <div>
-                <button onClick={this.prevVideo}>Previous</button>
+                <div id="nav">
+                    <button onClick={this.prevVideo}>Previous</button>
+                    <button onClink={this.allVideo}>All</button>
+                    <button onClick={this.nextVideo}>Next</button>
+                </div>
+
                 <ViewVideo activeVideo={this.state} />
-                <button onClick={this.nextVideo}>Next</button>
+                <addComment activeVideo={this.state} />
+
             </div>
         );
     }
@@ -73,45 +89,64 @@ var ViewVideo = React.createClass({
     render: function() {
         return (
             <div>
-                <h2>{this.props.activeVideo.title}</h2>
-                <iframe width="420" height="315" src={this.props.activeVideo.video} frameBorder="0" allowFullScreen></iframe>
-                <span>Rating: {this.props.activeVideo.rating}</span>
-                <span>Category: {this.props.activeVideo.category}</span>
+                <h2 id="videoTitle">{this.props.activeVideo.title}</h2>
+                <a id="videoPlayer" href={this.props.activeVideo.video}><div id="video"><img id="thumbnail" src={this.props.activeVideo.thumbnail} alt="" /></div></a>
+                <div id="rating">
+                    <span>Rating: {this.props.activeVideo.rating}</span>
+                </div>
+                <div id="category">
+                    <span>Category: {this.props.activeVideo.category}</span>
+                </div>
                 <div id="comments">{this.props.activeVideo.comments}</div>
             </div>
         )
     }
 });
 
+var ShowAllVideos = React.createClass({
+    if (pressed) {
+
+    },
+    render: function() {
+        return (<div>HELLO</div>)
+    }
+});
 // Under konstruktion
 var addComment = React.createClass({
     render: function () {
-        return (<div></div>)
+        return (
+            <div>
+                <h2>HELLO</h2>
+            </div>
+        )
     }
 });
 
 // DB
 var videos = [{
         title: 'Winter Barn',
-        url: 'https://www.youtube.com/embed/_xkn0ceDreo', //OBS KRÄVS SPECIELL EMBED URL
+        id: '_xkn0ceDreo',
+        url: 'https://m.youtube.com/watch?v=_xkn0ceDreo', //OBS KRÄVS SPECIELL EMBED URL /Den funkar inte på mobila eneheter, utan då försöker den tvinga en öppning av appen i stället
         rating: '3',
-        thumbnail: '',
+        thumbnail: 'http://img.youtube.com/vi/_xkn0ceDreo/0.jpg',
         category: 'Painting',
         comments: ['A winterbarn comment', 'fan händer nu']
     },
     {
         title: 'Sunset Oval',
-        url: 'https://www.youtube.com/embed/9xG6IzcGotI',
+        id: '9xG6IzcGotI',
+        url: 'https://m.youtube.com/watch?v=9xG6IzcGotI',
         rating: '5',
-        thumbnail: '',
+        thumbnail: 'http://img.youtube.com/vi/9xG6IzcGotI/0.jpg',
         category: '',
         comments: ['A sunsetoval comment']
     },
     {
         title: 'Lakeside Path',
-        url: 'https://www.youtube.com/embed/1yjGoJokbZg',
+        id: '1yjGoJokbZg',
+        url: 'https://m.youtube.com/watch?v=1yjGoJokbZg',
         rating: '2',
-        thumbnail: '',
+        thumbnail: 'http://img.youtube.com/vi/1yjGoJokbZg/0.jpg',
         category: '',
         comments: ['A lakesidepath comment']
     }
